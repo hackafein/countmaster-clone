18890cf48107c545bfcf2aebaf626d98cd259f0f
draqgamedev <draqgamedev@gmail.com>
2023-01-01 17:46:59 +0300

    player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -16,6 +16,9 @@ public class PlayerController : MonoBehaviour
     public int numberOfPlayerClones;
     public int totalNumberOfPlayerClones;
     // Start is called before the first frame update
+    public float speed = 10.0f;
+    private Rigidbody rb;
+    private float screenWidth;
 
     private void Awake()
     {
@@ -25,11 +28,41 @@ public class PlayerController : MonoBehaviour
     {
         totalNumberOfPlayerClones = transform.childCount - 1;
         player = gameObject.transform;
+        rb = GetComponent<Rigidbody>();
+
+        // Calculate the width of the screen in world coordinates
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float widthOrtho = Camera.main.orthographicSize * screenRatio;
+        screenWidth = widthOrtho * 2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 position = transform.position;
+        position.z += speed * Time.deltaTime;
+
+        // Check if the player is touching the screen
+        if (Input.touchCount > 0)
+        {
+            // Get the first touch
+            Touch touch = Input.GetTouch(0);
+
+            // Set the target position for the player
+            Vector3 targetPosition = transform.position;
+            targetPosition.x += touch.deltaPosition.x * 500000f;
+
+            // Move the player towards the target position
+            rb.MovePosition(Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime));;
+        }
+
+
+        // Limit the player's position to the edges of the screen
+        float leftEdge = -screenWidth / 2;
+        float rightEdge = screenWidth / 2;
+        position.x = Mathf.Clamp(position.x, leftEdge, rightEdge);
+        transform.position = position;
+
         if (fightStarted)
         {
             if(Enemy.instance.totalNumberEnemies == 0)
